41b02e68f402a624a581cd4fb3765fa7395e727a
trannam <namphongtran232@gmail.com>
2023-07-13 15:49:14 +0700

    update server to mock POST request

--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./db.json')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
+
 server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
