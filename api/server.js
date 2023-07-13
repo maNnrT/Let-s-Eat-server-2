02f4606df317365c5f8a882371aae65f67691dd5
trannam <namphongtran232@gmail.com>
2023-07-13 16:47:42 +0700

    update

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const clone = require('clone')
-const data = require('./db.json')
+const data = require('../db.json')
 const server = jsonServer.create()
 const isProductionEnv = process.env.NODE_ENV === 'production';
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+const router = jsonServer.router(isProductionEnv ? clone(data) : '..db.json', {
     _isFake: isProductionEnv
 })
 const middlewares = jsonServer.defaults()
